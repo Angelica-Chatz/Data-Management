29363138aa78786407c288857aafa9cd75a9d4fa
Angelica-Chatz <angelica6192@yahoo.gr>
2017-04-28 14:00:25 +0300

    Delete QueriesT3.sql

--- Task_3/QueriesT3.sql
+++
@@ -1,4 +0,0 @@
-select count(Therapy_Sessions.ID)/ count(distinct Patient_Case.ID) 
-as Avg_No_of_Therapy_Sessions_per_Case_out_of_all_recorded_cases
-from Patient_Case left join Therapy_Sessions
-on Patient_Case.ID= Therapy_Sessions.CaseID;
